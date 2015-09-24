86c4f1aa56d6be6b7b5b18fbae46fde302da7b04
Louy <me@l0uy.com>
2015-09-24 16:30:54 +0300

    Test cases for new errors

---
+++ test/bad-request.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+
+describe('BadRequestError', function() {
+  var BadRequestError = require('../bad-request');
+
+  it('should be an error', function() {
+    var error = new BadRequestError();
+
+    expect(error).to.be.an.instanceOf(Error);
+    expect(error).to.have.property('httpCode').and.to.equal(400);
+    expect(error).to.have.property('stack').and.to.contain('bad-request.js');
+  });
+
+  it('should support message', function() {
+    var msg = 'test';
+    var error = new BadRequestError(msg);
+
+    expect(error).to.have.property('message').and.to.equal(msg);
+  });
+
+  it('should support custom properties', function() {
+    var error = new BadRequestError('test', { test1: true });
+
+    expect(error).to.have.property('test1').and.to.equal(true);
+  });
+});
---
+++ test/unprocessable-entity.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+
+describe('UnprocessableEntityError', function() {
+  var UnprocessableEntityError = require('../unprocessable-entity');
+
+  it('should be an error', function() {
+    var error = new UnprocessableEntityError();
+
+    expect(error).to.be.an.instanceOf(Error);
+    expect(error).to.have.property('httpCode').and.to.equal(422);
+    expect(error).to.have.property('stack').and.to.contain('unprocessable-entity.js');
+  });
+
+  it('should support message', function() {
+    var msg = 'test';
+    var error = new UnprocessableEntityError(msg);
+
+    expect(error).to.have.property('message').and.to.equal(msg);
+  });
+
+  it('should support custom properties', function() {
+    var error = new UnprocessableEntityError('test', { test1: true });
+
+    expect(error).to.have.property('test1').and.to.equal(true);
+  });
+});
