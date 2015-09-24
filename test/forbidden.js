75d719ebddd6422441d20a864db67bb40696fe1d
Louy <me@l0uy.com>
2015-09-24 16:01:50 +0300

    More test cases

--- test/forbidden.js
+++ test/forbidden.js
@@ -1,25 +1,25 @@
 var expect = require('chai').expect;
 
-describe('NotFoundError', function() {
-  var NotFoundError = require('../not-found');
+describe('ForbiddenError', function() {
+  var ForbiddenError = require('../forbidden');
 
   it('should be an error', function() {
-    var error = new NotFoundError();
+    var error = new ForbiddenError();
 
     expect(error).to.be.an.instanceOf(Error);
-    expect(error).to.have.property('httpCode').and.to.equal(404);
-    expect(error).to.have.property('stack').and.to.contain('not-found.js');
+    expect(error).to.have.property('httpCode').and.to.equal(403);
+    expect(error).to.have.property('stack').and.to.contain('forbidden.js');
   });
 
   it('should support message', function() {
     var msg = 'test';
-    var error = new NotFoundError(msg);
+    var error = new ForbiddenError(msg);
 
     expect(error).to.have.property('message').and.to.equal(msg);
   });
 
   it('should support custom properties', function() {
-    var error = new NotFoundError('test', { test1: true });
+    var error = new ForbiddenError('test', { test1: true });
 
     expect(error).to.have.property('test1').and.to.equal(true);
   });
--- test/unauthorized.js
+++ test/unauthorized.js
@@ -1,25 +1,25 @@
 var expect = require('chai').expect;
 
-describe('NotFoundError', function() {
-  var NotFoundError = require('../not-found');
+describe('UnauthorizedError', function() {
+  var UnauthorizedError = require('../unauthorized');
 
   it('should be an error', function() {
-    var error = new NotFoundError();
+    var error = new UnauthorizedError();
 
     expect(error).to.be.an.instanceOf(Error);
-    expect(error).to.have.property('httpCode').and.to.equal(404);
-    expect(error).to.have.property('stack').and.to.contain('not-found.js');
+    expect(error).to.have.property('httpCode').and.to.equal(401);
+    expect(error).to.have.property('stack').and.to.contain('unauthorized.js');
   });
 
   it('should support message', function() {
     var msg = 'test';
-    var error = new NotFoundError(msg);
+    var error = new UnauthorizedError(msg);
 
     expect(error).to.have.property('message').and.to.equal(msg);
   });
 
   it('should support custom properties', function() {
-    var error = new NotFoundError('test', { test1: true });
+    var error = new UnauthorizedError('test', { test1: true });
 
     expect(error).to.have.property('test1').and.to.equal(true);
   });
