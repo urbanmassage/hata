8b47d1efc6ecadcfe957d1026200030014e6800a
Louy <louy08@gmail.com>
2015-11-26 21:37:08 +0200

    Update maps in scripts/map-coverage

--- scripts/map-coverage.ts
+++ scripts/map-coverage.ts
@@ -1,7 +1,17 @@
 const lcovSourcemap = require('lcov-sourcemap');
+const glob = require('glob');
 
-lcovSourcemap.writeLcov('./coverage/lcov.info', {
-  index: './index.js.map',
-}, '.', './coverage/lcov-mapped.info').then(function () {
+const IGNORE_PATTERNS = ['./node_modules', './test', './scripts']
+
+const maps = glob.sync('./**/*.js.map')
+.filter(path => !IGNORE_PATTERNS.some(pattern => path.indexOf(pattern) !== -1))
+.reduce((maps, file) => {
+  const base = file.substr(2, file.length - 9);
+  maps[base] = file;
+  return maps;
+}, {});
+
+lcovSourcemap.writeLcov('./coverage/lcov.info', maps, '.', './coverage/lcov-mapped.info')
+.then(function () {
   console.log('Coverage Mapped!');
 });
